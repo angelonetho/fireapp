071457f76d42af856f04c2e2e0368623d4f33349
Angelo-Netho <angelonetho@gmail.com>
2023-02-12 13:03:19 -0300

    Criando página de admin

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,9 +1,49 @@
+import { signOut } from 'firebase/auth'
+import { addDoc, collection } from 'firebase/firestore'
+import { useEffect, useState } from 'react'
+
+import { auth, db } from '../../firebaseConnection'
+
 import './admin.css'
 
 export default function Admin() {
+
+    const [tarefaInput, setTarefaInput] = useState("")
+
+
+    async function handleRegister(e) {
+        e.preventDefault()
+
+
+    }
+
+    async function handleLogout() {
+        await signOut(auth)
+    }
+
     return(
-        <div>
-            <h1>Admin</h1>
+        <div className='admin-container'>
+            <h1>Minhas tarefas</h1>
+
+            <form className='form' onSubmit={handleRegister}>
+                <textarea
+                    placeholder='Digite sua tarefa'
+                    value={tarefaInput}
+                    onChange={(e) => setTarefaInput(e.target.value)}
+                />
+
+                <button className='btn-register' type='submit'>Registrar tarefa</button>
+            </form>
+
+            <article className='list'>
+                <p>Estudar js e reactjs hj a noite</p>
+                <div>
+                    <button>Editar</button>
+                    <button className='btn-delete'>Concluir</button>
+                </div>
+            </article>
+
+            <button className='btn-logout' onClick={handleLogout}>Sair</button>
         </div>
     )
 }
