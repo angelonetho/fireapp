3905c2aa27f48e84557626343c0aba2da5ba2582
Angelo-Netho <angelonetho@gmail.com>
2023-02-11 23:09:56 -0300

    Criando página home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,50 @@
 
+import { useState } from 'react'
+
+import './home.css'
+
+import { Link } from 'react-router-dom'
+
 export default function Home(){
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  function handleLogin(e) {
+    e.preventDefault()
+    
+    if(email !== '' && password !== '') {
+      alert("te")
+    } else {
+      alert("Preencha todos os campos")
+    }
+  }
+
   return(
-    <div>
-      <h1>Pagina HOME</h1>
+    <div className='home-container'>
+      <h1>Lista de tarefas</h1>
+      <span>Gerencie sua agenda de forma fácil!</span>
+      
+      <form className='form' onSubmit={handleLogin}>
+        <input
+          type='text'
+          placeholder='Email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type='password'
+          placeholder='Senha'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type='submit'>Acessar</button>
+      </form>
+
+      <Link to="/register" className='button-link'>
+        Não possui uma conta? Cadastre-se
+      </Link>
     </div>
   )
 }
