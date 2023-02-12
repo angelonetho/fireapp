c2f20dec96967a3d09b3b2a27337290bd5a21908
Angelo-Netho <angelonetho@gmail.com>
2023-02-12 15:05:21 -0300

    Buscando dados do banco

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,5 +1,5 @@
 import { signOut } from 'firebase/auth'
-import { addDoc, collection } from 'firebase/firestore'
+import { addDoc, collection, doc, onSnapshot, orderBy, query, where } from 'firebase/firestore'
 import { useEffect, useState } from 'react'
 
 import { auth, db } from '../../firebaseConnection'
@@ -10,11 +10,34 @@ export default function Admin() {
 
     const [tarefaInput, setTarefaInput] = useState("")
     const [user, setUser] = useState({})
+    const [tarefas, setTarefas] = useState([])
 
     useEffect(() => {
         async function loadTarefas() {
             const userDetail = localStorage.getItem("@detailUser")
             setUser(JSON.parse(userDetail))
+
+            if(userDetail) {
+                const data = JSON.parse(userDetail)
+
+                const tarefaRef = collection(db, "tarefas")
+                const q = query(tarefaRef, orderBy("created", "desc"), where("userUid", "==", data?.uid))
+                const unsub = onSnapshot(q, (snapshot) => {
+                    let lista = []
+
+                    snapshot.forEach((doc) => {
+                        lista.push({
+                            id: doc.id,
+                            tarefa: doc.data().tarefa,
+                            userUid: doc.data().userUid
+                        })
+                    })
+
+                    console.log(lista)
+                    setTarefas(lista)
+                })
+
+            }
         }
 
         loadTarefas()
