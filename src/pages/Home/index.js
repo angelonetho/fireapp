3c3766b4a12e0d4fccdf8ebe9b4f9447ef71cd0c
Angelo-Netho <angelonetho@gmail.com>
2023-02-11 23:55:01 -0300

    Criando página register

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,16 +5,31 @@ import './home.css'
 
 import { Link } from 'react-router-dom'
 
+import { auth } from '../../firebaseConnection'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+
+import { useNavigate } from 'react-router-dom'
+
 export default function Home(){
 
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
-  function handleLogin(e) {
+  const navigate = useNavigate()
+
+  async function handleLogin(e) {
     e.preventDefault()
     
     if(email !== '' && password !== '') {
-      alert("te")
+      
+      await signInWithEmailAndPassword(auth, email, password)
+      .then(()=> {
+        navigate('/admin', { replace: true})
+      })
+      .catch(()=> {
+        console.log('erro login')
+      })
+
     } else {
       alert("Preencha todos os campos")
     }
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,8 +1,63 @@
 
+import { useState } from 'react'
+
+import { Link } from 'react-router-dom'
+
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { auth }from '../../firebaseConnection'
+
+import { useNavigate } from 'react-router-dom'
+
 export default function Register(){
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const navigate = useNavigate()
+
+  async function handleRegister(e) {
+    e.preventDefault()
+    
+    if(email !== '' && password !== '') {
+      
+      await createUserWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        navigate('/admin', { replace: true})
+      })
+      .catch(() => {
+        console.log("erro cadastro")
+      })
+
+
+    } else {
+      alert("Preencha todos os campos")
+    }
+  }
+
   return(
-    <div>
-      <h1>Pagina Register</h1>
+    <div className='home-container'>
+      <h1>Cadastre-se</h1>
+      <span>Vamos criar sua conta!</span>
+      
+      <form className='form' onSubmit={handleRegister}>
+        <input
+          type='text'
+          placeholder='Email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type='password'
+          placeholder='Senha'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type='submit'>Cadastrar</button>
+      </form>
+
+      <Link to="/" className='button-link'>
+        Já possuí uma conta? Faça login!
+      </Link>
     </div>
   )
 }
